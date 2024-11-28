f8df7cc9a7d6f150b8aee4917a1ed9c3f293b8fc
cjlevec <cjlevec@gmail.com>
2024-11-28 18:39:10 -0500

    fixing transformations to accomodate semantic segm.

--- fcn_resnet50.py
+++ fcn_resnet50.py
@@ -4,21 +4,31 @@ import torchvision
 import torchvision.models as models
 import torchvision.transforms as transforms
 import torch.nn.functional as F
-from mpl_toolkits.mplot3d.proj3d import transform
+from torchvision.models.segmentation import fcn_resnet50, FCN_ResNet50_Weights
 from torchvision.datasets import VOCSegmentation
 from torch.utils.data import DataLoader
 
 
-transform =transforms.Compose([
+# Define transforms for image and target separately
+image_transform = transforms.Compose([
     transforms.Resize((256, 256)),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+])
+
+target_transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.PILToTensor(),
+    transforms.Lambda(lambda t: torch.clamp(t, 0, 20).long())  # Clamp maps pixels part of background class (class 255) to 20
 ])
 
 # Load PASCAL VOC 2012 dataset
-dataset = VOCSegmentation(root='./data', year='2012', image_set='train', download=True, transform=transform)
+dataset = VOCSegmentation(root='./data', year='2012', image_set='train', download=True, transform=image_transform, target_transform= target_transform)
 
 # Make instance of the fcn_resnet50 model (Pretrained on COCO VOC)
-model = models.segmentation.fcn_resnet50(weights= 'FCN_resnet50_weights.pth', pretrained=True, progress=True, num_classes=20)
+model = models.segmentation.fcn_resnet50(weights= 'DEFAULT', pretrained=True, progress=True)
+# Need to modify the last layer of the classifier to output 21 classes for PASCAL
+# (20 + 1 class for the background)
+model.classifier[4] = torch.nn.Conv2d(512, 21, kernel_size=(1, 1), stride=(1, 1))
 
 # DataLoader
 dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
@@ -29,8 +39,8 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 # Send model to GPU if available
 device = 'cpu'
-if torch.cuda.is_available():
-    device = 'cuda'
+"""if torch.cuda.is_available():
+    device = 'cuda'"""
 
 model = model.to(device)
 
@@ -42,7 +52,7 @@ for epoch in range (50):
         labels = labels.to(device)
         optimizer.zero_grad()
         outputs = model(inputs)
-        loss = criterion(outputs, labels)
+        loss = criterion(outputs['out'], labels.squeeze(1).long())
         loss.backward()
         optimizer.step()
     print(f"Epoch {epoch+1}, Loss: {loss.item():.4f}")
